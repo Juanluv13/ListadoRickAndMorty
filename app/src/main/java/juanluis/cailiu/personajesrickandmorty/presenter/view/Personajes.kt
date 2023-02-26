b593a4108ec6dfc9df52de99a42d738bd3add22a
JuanLuis <juanluis13cai@gmail.com>
2023-02-26 16:23:09 +0100

    editar cambios pero no en la base de datos

--- app/src/main/java/juanluis/cailiu/personajesrickandmorty/presenter/view/Personajes.kt
+++ app/src/main/java/juanluis/cailiu/personajesrickandmorty/presenter/view/Personajes.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.view.MenuItem
+import android.view.View
 import coil.load
 import juanluis.cailiu.personajesrickandmorty.R
 import juanluis.cailiu.personajesrickandmorty.databinding.ActivityMainBinding
@@ -33,6 +34,30 @@ class Personajes : AppCompatActivity() {
         binding.tvNameCharacter.text = name
         binding.imgCharacter.load(image)
 
+        binding.btnEditarDatos.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                binding.textBoxContainerEditable.visibility=View.VISIBLE
+                binding.textBoxContainer.visibility=View.GONE
+                binding.edEspecie.text = Editable.Factory.getInstance().newEditable(species)
+                binding.edGenero.text = Editable.Factory.getInstance().newEditable(gender)
+                binding.edFechaCreacion.text = Editable.Factory.getInstance().newEditable(created)
+                binding.edName.text =Editable.Factory.getInstance().newEditable(name)
+                binding.btnGuardar.visibility=View.VISIBLE
+                binding.btnEditarDatos.visibility=View.GONE
+            }
+        })
+        binding.btnGuardar.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                binding.tvSpecieCharacter.text = binding.edEspecie.text
+                binding.tvGenderCharacter.text = binding.edGenero.text
+                binding.tvCreatedCharacter.text =binding.edFechaCreacion.text
+                binding.tvNameCharacter.text = binding.edName.text
+                binding.btnGuardar.visibility=View.GONE
+                binding.btnEditarDatos.visibility=View.VISIBLE
+                binding.textBoxContainerEditable.visibility=View.GONE
+                binding.textBoxContainer.visibility=View.VISIBLE
+            }
+        })
     }
 
     private fun getDate(created: String?): String {
@@ -42,6 +67,7 @@ class Personajes : AppCompatActivity() {
         val outputDate = outputFormat.format(date)
         return outputDate
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
