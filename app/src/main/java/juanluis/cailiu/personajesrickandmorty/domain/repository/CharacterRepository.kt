8963a77d64abab0ce5afb97d34487bddbfd85848
JuanLuis <juanluis13cai@gmail.com>
2023-02-26 15:45:01 +0100

    RemoverItem on swipe

--- app/src/main/java/juanluis/cailiu/personajesrickandmorty/domain/repository/CharacterRepository.kt
+++ app/src/main/java/juanluis/cailiu/personajesrickandmorty/domain/repository/CharacterRepository.kt
@@ -3,6 +3,9 @@ package juanluis.cailiu.personajesrickandmorty.domain.repository
 import juanluis.cailiu.personajesrickandmorty.data.source.local.CharacterDao
 import juanluis.cailiu.personajesrickandmorty.data.source.local.entity.CharacterEntity
 import juanluis.cailiu.personajesrickandmorty.data.source.remote.dto.Character
+import juanluis.cailiu.personajesrickandmorty.domain.model.CharacterModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 interface CharacterRepository {
     suspend fun getCharacter(): Character
@@ -10,4 +13,9 @@ interface CharacterRepository {
     suspend fun characterDelete()
     suspend fun characterSelect(): List<CharacterEntity>
 
+    companion object {
+        suspend fun deleteCharacter(characterModel: CharacterModel)= withContext(Dispatchers.IO) {
+            CharacterDao.deleteCharacter(characterModel)
+        }
+    }
 }
--- app/src/main/java/juanluis/cailiu/personajesrickandmorty/presenter/viewmodel/CharacterViewModel.kt
+++ app/src/main/java/juanluis/cailiu/personajesrickandmorty/presenter/viewmodel/CharacterViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import juanluis.cailiu.personajesrickandmorty.domain.usecase.CharacterUseCase
 import juanluis.cailiu.personajesrickandmorty.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import juanluis.cailiu.personajesrickandmorty.domain.model.CharacterModel
 import juanluis.cailiu.personajesrickandmorty.domain.repository.CharacterRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -20,7 +21,9 @@ class CharacterViewModel @Inject constructor(private val useCase: CharacterUseCa
     init {
         getCharacters()
     }
-
+    fun deleteCharacter(characterModel: CharacterModel) = viewModelScope.launch {
+        CharacterRepository.deleteCharacter(characterModel)
+    }
     private fun getCharacters() = viewModelScope.launch(Dispatchers.IO) {
         useCase().collect{
             when(it){
